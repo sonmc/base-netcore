fd8b4c620c0b2d9cdd7a111d5cd405a6d7b37ac3
sonmc <sonmc90@gmail.com>
2023-08-03 16:56:23 +0700

    add docker

--- Base.Core/DataContext.cs
+++ Base.Core/DataContext.cs
@@ -26,7 +26,7 @@ namespace Base.Core
             DbContextOptionsBuilder optionsBuilder
         )
         {
-            var connectionString = "server=localhost;port=3306;database=test_db10;uid=root;password=123456";
+            var connectionString = "server=localhost;port=3306;database=base_core;uid=root;password=123456";
             optionsBuilder.UseMySql(connectionString, ServerVersion.Parse("8.0.28-mysql"), builder =>
            {
                builder.EnableRetryOnFailure(5, TimeSpan.FromSeconds(10), null);
---
+++ Dockerfile
@@ -0,0 +1,12 @@
+#Build Stage
+FROM mcr.microsoft.com/dotnet/sdk:6.0 AS build
+WORKDIR /base-netcore
+COPY . .
+RUN dotnet restore "./Base.Application/Base.Application.csproj" --disable-parallel
+RUN dotnet publish "./Base.Application/Base.Application.csproj" -c release -o /app --no-restore
+# Serve Stage
+FROM mcr.microsoft.com/dotnet/aspnet:6.0
+WORKDIR /app
+COPY --from=build /app ./
+EXPOSE 5000
+ENTRYPOINT [ "dotnet", "Base.Application.dll" ]
